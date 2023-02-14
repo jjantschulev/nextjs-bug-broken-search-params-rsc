3674e716e2e20ea44a02839a57a4424d21043564
Jordan Jantschulev <j.jantschulev@gmail.com>
2023-02-14 13:13:30 +1100

    push

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const url = req.url;
+  console.log("MIDDLEWARE req.url:", url);
   const newReqHeaders = new Headers(req.headers);
   newReqHeaders.set("x-request-url", url);
   const response = NextResponse.next({
