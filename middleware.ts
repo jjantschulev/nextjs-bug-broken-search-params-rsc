4b036ca476028bf07350b94e4360e815e9f01fc1
Jordan Jantschulev <j.jantschulev@gmail.com>
2023-02-14 15:00:07 +1100

    push

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,8 @@ export async function middleware(req: NextRequest) {
   console.log("MIDDLEWARE req.url:", url);
   const response = NextResponse.next();
   response.headers.set("x-request-url", url);
+  response.headers.set("x-request-pathname", req.nextUrl.pathname ?? "null");
+  response.headers.set("x-request-search", req.nextUrl.search ?? "null");
+  response.headers.set("x-request-href", req.nextUrl.href ?? "null");
   return response;
 }
