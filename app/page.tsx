d17faf9d3c986661ea0fc26cc5bba321642003b5
Jordan Jantschulev <j.jantschulev@gmail.com>
2023-02-14 12:26:34 +1100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,29 @@
 "use client";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  const params = useSearchParams();
-  const pathname = usePathname();
-  const router = useRouter();
+  const [path, setPath] = useState("/api/?query=world");
+  const [result, setResult] = useState("");
 
-  const toggleParams = useCallback(() => {
-    const newParams = new URLSearchParams(params);
-    if (newParams.has("foo")) {
-      newParams.delete("foo");
-    } else {
-      newParams.set("foo", "bar");
-    }
-    const newPath = `${pathname}?${newParams}`;
-    router.replace(newPath);
-  }, [params, location]);
+  const makeRequest = useCallback(async () => {
+    const request = await fetch(path);
+    const reqUrl = request.headers.get("x-request-url");
+    setResult(reqUrl);
+  }, [path]);
 
   return (
     <div>
       <h1>Home</h1>
-      <p>Pathname: {pathname}</p>
-      <p>Params: {JSON.stringify([...params.entries()])}</p>
-      <button onClick={toggleParams}>Toggle Params</button>
+      <input
+        type="text"
+        value={path}
+        onChange={(e) => setPath(e.target.value)}
+      />
+      <p>
+        <kbd>x-request-url: {result}</kbd>
+      </p>
+      <button onClick={makeRequest}>Make Request</button>
     </div>
   );
 }
